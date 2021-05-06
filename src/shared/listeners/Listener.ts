c741e2ee3cf52e5caf4a1b1673caa25707b2dfa7
jbofill10 <juan.bofill10@gmail.com>
2021-05-05 23:19:42 -0400

    Add super class for main proc listeners

---
+++ src/shared/listeners/Listener.ts
@@ -0,0 +1,18 @@
+import { App, BrowserWindow } from "electron";
+import { Store } from "../Store";
+
+/**
+ * Used to keep instances hold instance of store, main window, and app
+ *  for child listeners to use
+ */
+export class Listener {
+	mainWindow: BrowserWindow = null;
+	store: Store = null;
+	app: App = null;
+
+	constructor(mainWindow: BrowserWindow, app: App){
+		this.mainWindow = mainWindow;
+		this.app = app;
+		this.store = new Store();
+	}
+}
